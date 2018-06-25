5ccff70d93bb8325c93e346734c8af65abebb0b6
Mayur <mayur.nadiya.php@gmail.com>
2018-06-26 00:32:19 +0530

    Remove var folder from git

--- var/cache/se/jms_security/AppBundle-Controller-SidebarController.cache.php
+++
@@ -1,1 +0,0 @@
-<?php return unserialize('C:46:"JMS\\SecurityExtraBundle\\Metadata\\ClassMetadata":103:{a:5:{i:0;s:38:"AppBundle\\Controller\\SidebarController";i:1;a:0:{}i:2;a:0:{}i:3;a:0:{}i:4;i:1529168270;}}');
